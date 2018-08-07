77a08e644c47d07cfd31933ee6bb4dcffa384b38
hasanuralam <42185860+hasanuralam@users.noreply.github.com>
2018-08-08 00:31:00 +0600

    Add files via upload

---
+++ active.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+   $(window).on("load", function(){
+        $(".factorian-site-preloader-warp").fadeOut(500);
+        
+     });
+   new WOW().init();
+
+});
+  
+
+
+
+    
+   
+
+
+
+
+
